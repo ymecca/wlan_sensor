df4473cecb2482ac64cc25f818a4376e07b9e6da
Your Name <you@example.com>
2020-02-27 21:30:09 -0500

    v0.3

--- WLAN_INFO.py
+++ WLAN_INFO.py
@@ -5,4 +5,4 @@ WLAN_SNR=os.system('iwconfig wlan0 | grep Rate | awk \'{print$2\'}')
 WLAN_SIGNAL=os.system('iwconfig wlan0 | grep Quality | awk \'{print$4\'}')
 WLAN_IP=os.system('ifconfig wlan0 | grep mask | awk \'{print$2\'}')
 
-print (WLAN_IP WLAN_SIGNAL WLAN_SNR)
+print (WLAN_IP , WLAN_SIGNAL , WLAN_SNR)
