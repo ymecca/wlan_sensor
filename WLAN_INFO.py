e2047981f76a037a454a4e77ab743c6e96289a7d
Your Name <you@example.com>
2020-02-27 22:14:46 -0500

    v1.2

--- WLAN_INFO.py
+++ WLAN_INFO.py
@@ -2,12 +2,15 @@ import os
 import subprocess as sp
 import csv
 
-WLAN_SNR=sp.getoutput('iwconfig wlan0 | grep Rate | awk \'{print$2\'}')
-WLAN_SIGNAL=sp.getoutput('iwconfig wlan0 | grep Quality | awk \'{print$4\'}')
-WLAN_IP=sp.getoutput('ifconfig wlan0 | grep mask | awk \'{print$2\'}')
+WLAN_RATE = sp.getoutput('iwconfig wlan0 | grep Rate | awk \'{print$2\'}')
+WLAN_SIGNAL = sp.getoutput('iwconfig wlan0 | grep Quality | awk \'{print$4\'}')
+WLAN_IP = sp.getoutput('ifconfig wlan0 | grep mask | awk \'{print$2\'}')
+
+WLAN_RATE = WLAN_RATE.replace("Rate=","")
+WLAN_SIGNAL = WLAN_SIGNAL.replace("level=","")
 
 with open('SENSOR.csv', 'w', newline='') as csvfile:
     PRINTER = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    PRINTER.writerow([WLAN_IP]+[WLAN_SNR]+[WLAN_SIGNAL])
+    PRINTER.writerow([WLAN_IP]+[WLAN_RATE]+[WLAN_SIGNAL])
 
