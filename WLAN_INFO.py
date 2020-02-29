1796873ec95d99240eccb7f18dda8034344474cf
Your Name <you@example.com>
2020-02-29 11:52:18 -0500

    JSON VERSION

--- WLAN_INFO.py
+++ WLAN_INFO.py
@@ -1,16 +1,25 @@
 import os
 import subprocess as sp
-import csv
+import json
 
 WLAN_RATE = sp.getoutput('iwconfig wlan0 | grep Rate | awk \'{print$2\'}')
 WLAN_SIGNAL = sp.getoutput('iwconfig wlan0 | grep Quality | awk \'{print$4\'}')
-WLAN_IP = sp.getoutput('ifconfig wlan0 | grep mask | awk \'{print$2\'}')
+WLAN_IPV4 = sp.getoutput('ifconfig wlan0 | grep mask | awk \'{print$2\'}')
+WLAN_TIME = sp.getoutput('date')
 
 WLAN_RATE = WLAN_RATE.replace("Rate=","")
 WLAN_SIGNAL = WLAN_SIGNAL.replace("level=","")
+WLAN_TIME = WLAN_TIME.replace(" ","_")
 
-with open('DATABASE.csv', 'w', newline='') as csvfile:
-    PRINTER = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    PRINTER.writerow([WLAN_IP]+[WLAN_RATE]+[WLAN_SIGNAL])
+DATA = {}
+DATA['WLAN'] = []
+DATA['WLAN'].append({
+    'WLAN_RATE(Mbps)': WLAN_RATE,
+    'WLAN_SIGNAL(dBM)': WLAN_SIGNAL,
+    'WLAN_IPV4': WLAN_IPV4,
+    'WLAN_TIME': WLAN_TIME
 
+})
+
+with open('DATABASE.json', 'w') as outfile:
+    json.dump(DATA, outfile)
