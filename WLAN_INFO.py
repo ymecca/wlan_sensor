d3f33f7a523f4a0313b352ac3f7ff1080a22bb6e
Your Name <you@example.com>
2020-02-29 20:32:32 -0500

    ADDED_TIME

--- WLAN_INFO.py
+++ WLAN_INFO.py
@@ -6,6 +6,7 @@ WLAN_RATE = sp.getoutput('iwconfig wlan0 | grep Rate | awk \'{print$2\'}')
 WLAN_SIGNAL = sp.getoutput('iwconfig wlan0 | grep Quality | awk \'{print$4\'}')
 WLAN_IPV4 = sp.getoutput('ifconfig wlan0 | grep mask | awk \'{print$2\'}')
 WLAN_TIME = sp.getoutput('date')
+WLAN_HARDWARE_MAC = sp.getoutput('ifconfig wlan0 | grep ether | awk \'{print$2\'}')
 
 WLAN_RATE = WLAN_RATE.replace("Rate=","")
 WLAN_SIGNAL = WLAN_SIGNAL.replace("level=","")
@@ -14,6 +15,7 @@ WLAN_TIME = WLAN_TIME.replace(" ","_")
 DATA = {}
 DATA['WLAN'] = []
 DATA['WLAN'].append({
+    'WLAN_HARDWARE_MAC': WLAN_HARDWARE_MAC,
     'WLAN_RATE(Mbps)': WLAN_RATE,
     'WLAN_SIGNAL(dBM)': WLAN_SIGNAL,
     'WLAN_TIME': WLAN_TIME,
