6a670db470eccacbcf2fe250df8243ac0f716754
Your Name <you@example.com>
2020-03-02 21:24:11 -0500

    JSON_SCP

--- MAIN.py
+++ MAIN.py
@@ -2,6 +2,10 @@ import time
 import WLAN_INFO
 import WLAN_CONNECTOR
 
-WLAN_INFO
-WLAN_CONNECTOR
-time.sleep(10)
+while True:
+    print("Gathering WLAN Information from OS")
+    WLAN_INFO
+    print("Transmit file to the Collector Server")
+    WLAN_CONNECTOR
+    print("Countdown 5sec...")
+    time.sleep(5)
