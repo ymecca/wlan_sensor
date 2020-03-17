885e8560121a4e297296604d263ca17313c50af8
Your Name <you@example.com>
2020-03-17 09:01:02 -0400

    ADDED TRY IN MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -5,20 +5,27 @@ import WLAN_JSON_MERGE
 import os
 import subprocess as sp
 
+
+def WLAN_SERVER_CONNECTOR():
+    print("TRYING TO TRANSMIT FILE TO THE COLLECTOR SERVER")
+    try:
+        WLAN_CONNECTOR.WLAN_SCP()
+        print("----CONNECTION SUCCESSFUL!")
+        print("CLEANING JSON FILES\n\n#################################################")
+        sp.getoutput('rm /home/pi/wlan_sensor/client/DATABASE/*.*')
+    except:
+        print("CONNECTION FAILED. SKIPPING\n\n#################################################")
+
 while True:
-    print("Gathering WLAN Information from OS")
+    print("\n\n#################################################\n\nCOLLECTING WLAN INFORMATION FROM THE OS. PLEASE WAIT 60s")
 
-
-    i = 0
-    while i < 15:
+    i = 1
+    while i < 16:
         WLAN_INFO.WLAN_PROCESS()
         time.sleep(4)
-        print(i)
+        print("---- COLLECTED "+str(i)+"/15")
         i +=1
-    
-    print("Merging JSON Files")
-    WLAN_JSON_MERGE.WLAN_MERGE() 
-    print("Transmit file to the Collector Server")
-    WLAN_CONNECTOR.WLAN_SCP()
-    print("Cleaning JSON Files")
-    sp.getoutput('rm /home/pi/wlan_sensor/client/DATABASE/*.*')   
+
+    print("MERGING JSON FILES")
+    WLAN_JSON_MERGE.WLAN_MERGE()
+    WLAN_SERVER_CONNECTOR()    
