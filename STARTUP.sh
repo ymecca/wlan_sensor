77dd126b8154c59ae4a11940e3cda71446b2af6e
Your Name <you@example.com>
2020-03-19 08:53:19 -0400

    updated STARTUP.sh

--- STARTUP.sh
+++ STARTUP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /home/pi/environment/wlan_sensor/bin/activate
-nohup python /home/pi/wlan_sensor/client/MAIN.py &
+nohup python -u /home/pi/wlan_sensor/client/MAIN.py > /home/pi/wlan_sensor/client/LOG/LOGFILE.log &
