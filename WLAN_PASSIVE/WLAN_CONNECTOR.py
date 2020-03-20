6b962ce8916bfa73b76fd2758457056be30f3c7a
Your Name <you@example.com>
2020-03-20 09:20:45 -0400

    CHANGE PASSIVE DATABASE FILE

--- WLAN_PASSIVE/WLAN_CONNECTOR.py
+++ WLAN_PASSIVE/WLAN_CONNECTOR.py
@@ -11,7 +11,7 @@ def WLAN_SCP():
         ssh.connect(WLAN_SERVER)
         # SCPCLient takes a paramiko transport as an argument
         scp = SCPClient(ssh.get_transport())
-        scp.put('/home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/DATABASE.json', '/home/devnet/wlan_sensor/client/DATABASE.json')
+        scp.put('/home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/DATABASE_PASSIVE.json', '/home/devnet/wlan_sensor/client/DATABASE_PASSIVE.json')
         scp.close()
 
 if __name__ == '__main__':
--- WLAN_PASSIVE/WLAN_JSON_MERGE.py
+++ WLAN_PASSIVE/WLAN_JSON_MERGE.py
@@ -4,7 +4,7 @@ import glob
 def WLAN_MERGE():
 
     WLAN_READ_JSON = glob.glob("/home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/*.tempjson")
-    with open ("/home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/DATABASE.json", "w") as jsonfile:
+    with open ("/home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/DATABASE_PASSIVE.json", "w") as jsonfile:
         jsonfile.write('[{}]'.format(
             ','.join([open(f, "r").read() for f in WLAN_READ_JSON])))
 
