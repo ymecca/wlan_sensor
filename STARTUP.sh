6d33e680be33e44db8c9229266f29bd6b84e8a1e
Your Name <you@example.com>
2020-03-20 08:17:52 -0400

    Organized the main folder

--- STARTUP.sh
+++ STARTUP.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source /home/pi/environment/wlan_sensor/bin/activate
-nohup python -u /home/pi/wlan_sensor/client/MAIN.py > /home/pi/wlan_sensor/client/LOG/LOGFILE.log &
+nohup python -u /home/pi/wlan_sensor/client/WLAN_PASSIVE/MAIN.py > /home/pi/wlan_sensor/client/WLAN_PASSIVE/LOG/LOGFILE.log &
+#nohup python -u /home/pi/wlan_sensor/client/WLAN_ACTIVE/MAIN.py > /home/pi/wlan_sensor/client/WLAN_ACTIVE/LOG/LOGFILE.log &
