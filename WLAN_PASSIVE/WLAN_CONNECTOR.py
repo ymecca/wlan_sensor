8824108f32c70b61cae417b148a8df86fb3608f3
Your Name <you@example.com>
2020-03-25 09:30:18 -0400

    Swapped Paramiko to Kernel SCP

--- WLAN_PASSIVE/WLAN_CONNECTOR.py
+++ WLAN_PASSIVE/WLAN_CONNECTOR.py
@@ -1,18 +1,10 @@
-from paramiko import SSHClient
-from scp import SCPClient
 import os
 import subprocess as sp
 
 
 def WLAN_SCP():
         WLAN_SERVER = sp.getoutput('avahi-resolve -4 --name wlan-server.local | awk \'{print $2}\'')
-        ssh = SSHClient()
-        ssh.load_system_host_keys()
-        ssh.connect(WLAN_SERVER)
-        # SCPCLient takes a paramiko transport as an argument
-        scp = SCPClient(ssh.get_transport())
-        scp.put('/home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/DATABASE_PASSIVE.json', '/home/devnet/wlan_sensor/client/DATABASE_PASSIVE.json')
-        scp.close()
+        sp.getoutput('scp -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/pi/wlan_sensor/client/WLAN_PASSIVE/DATABASE/DATABASE_PASSIVE.json '+WLAN_SERVER+':/home/devnet/wlan_sensor/client/DATABASE_PASSIVE.json')
 
 if __name__ == '__main__':
         WLAN_SCP()
